b00c04ef2fdca4509081c52fbed84d25fed2d728
mags <ranier.magno@snsofttech.ph>
2025-03-24 17:47:11 +0800

    TESTTT

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,9 @@
-import "./App.css";
-import { useEffect, useRef } from "react";
-import { useSearchParams } from "react-router-dom";
-import WebApp from "@twa-dev/sdk";
+import React from "react";
 
 const App = () => {
-  const divRef = useRef<HTMLDivElement | null>(null);
-  const [searchParams] = useSearchParams();
-
-  const onfocus = () => {
-    console.log("FOCUSED");
-    setTimeout(() => {
-      WebApp.expand();
-    }, 500);
-  };
-
-  useEffect(() => {
-    if (WebApp) {
-      WebApp.expand();
-    }
-    // if (!divRef.current) return;
-    // const viewport = searchParams.get("viewport");
-    // // alert("TESTTTTT");
-    // // alert(viewport);
-    // console.log("TESTTT");
-    // if (viewport) {
-    //   // alert(`${viewport} HATDOG`);
-    //   console.log("WHY ARE YOU RUNNING?");
-    //   divRef.current.style.height = `${viewport}vh`;
-    //   divRef.current.style.backgroundColor = "red";
-    // }
-    // document.querySelectorAll("input, textarea").forEach((el) => {
-    //   el.addEventListener("focus", () => {
-    //     alert("FOCUSED");
-    //     setTimeout(() => {
-    //       WebApp.expand();
-    //     }, 500);
-    //   });
-    // });
-  }, []);
-
   return (
-    <div className="container" ref={divRef}>
-      <div className="header">HEADER</div>
-
-      <div className="body">
-        <div>
-          <input type="text" onFocus={onfocus} />
-        </div>
-        <div>
-          <input type="text" onFocus={onfocus} />
-        </div>
-      </div>
-      <div className="footer">FOOTER</div>
+    <div>
+      <div>test</div>
     </div>
   );
 };
