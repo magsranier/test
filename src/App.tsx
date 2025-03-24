306005e7f68587f94f0b8a331e2502c24fdc0db5
snsoft-rona <ronalyn.taguiam@snsofttech.ph>
2025-03-24 17:29:16 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import "./App.css";
 import { useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
+import WebApp from "@twa-dev/sdk";
 
 const App = () => {
   const divRef = useRef<HTMLDivElement | null>(null);
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    if (!divRef.current) return;
-    const viewport = searchParams.get("viewport");
+    // if (!divRef.current) return;
+    // const viewport = searchParams.get("viewport");
 
-    // alert("TESTTTTT");
-    // alert(viewport);
+    // // alert("TESTTTTT");
+    // // alert(viewport);
 
-    console.log("TESTTT");
+    // console.log("TESTTT");
 
-    if (viewport) {
-      // alert(`${viewport} HATDOG`);
-      console.log("WHY ARE YOU RUNNING?");
-      divRef.current.style.height = `${viewport}vh`;
-      divRef.current.style.backgroundColor = "red";
-    }
+    // if (viewport) {
+    //   // alert(`${viewport} HATDOG`);
+    //   console.log("WHY ARE YOU RUNNING?");
+    //   divRef.current.style.height = `${viewport}vh`;
+    //   divRef.current.style.backgroundColor = "red";
+    // }
+
+    document.querySelectorAll("input, textarea").forEach((el) => {
+      el.addEventListener("focus", () => {
+        setTimeout(() => {
+          WebApp.expand();
+        }, 500);
+      });
+    });
   }, []);
 
   return (
