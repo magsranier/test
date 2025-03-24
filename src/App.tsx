962c175ffc36f1eeb8c3a219d1ccce4ff12f5a0e
snsoft-rona <ronalyn.taguiam@snsofttech.ph>
2025-03-24 17:33:07 +0800

    add expand

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,33 @@ const App = () => {
   const divRef = useRef<HTMLDivElement | null>(null);
   const [searchParams] = useSearchParams();
 
+  const onfocus = () => {
+    alert("FOCUSED");
+    setTimeout(() => {
+      WebApp.expand();
+    }, 500);
+  };
+
   useEffect(() => {
     // if (!divRef.current) return;
     // const viewport = searchParams.get("viewport");
-
     // // alert("TESTTTTT");
     // // alert(viewport);
-
     // console.log("TESTTT");
-
     // if (viewport) {
     //   // alert(`${viewport} HATDOG`);
     //   console.log("WHY ARE YOU RUNNING?");
     //   divRef.current.style.height = `${viewport}vh`;
     //   divRef.current.style.backgroundColor = "red";
     // }
-
-    document.querySelectorAll("input, textarea").forEach((el) => {
-      el.addEventListener("focus", () => {
-        alert("FOCUSED");
-        setTimeout(() => {
-          WebApp.expand();
-        }, 500);
-      });
-    });
+    // document.querySelectorAll("input, textarea").forEach((el) => {
+    //   el.addEventListener("focus", () => {
+    //     alert("FOCUSED");
+    //     setTimeout(() => {
+    //       WebApp.expand();
+    //     }, 500);
+    //   });
+    // });
   }, []);
 
   return (
@@ -38,10 +41,10 @@ const App = () => {
       <div className="header">HEADER</div>
 
       <div>
-        <input type="text" />
+        <input type="text" onFocus={onfocus} />
       </div>
       <div>
-        <input type="text" />
+        <input type="text" onFocus={onfocus} />
       </div>
       <div className="body"></div>
       <div className="footer">FOOTER</div>
