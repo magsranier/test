fe1c6eb1027f76b03b4568dd1773042eae9d424d
mags <ranier.magno@snsofttech.ph>
2025-03-24 17:50:26 +0800

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import "./App.css";
+import { useEffect, useRef } from "react";
+import WebApp from "@twa-dev/sdk";
 
 const App = () => {
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  const onfocus = () => {
+    console.log("FOCUSED");
+    setTimeout(() => {
+      WebApp.expand();
+    }, 500);
+  };
+
+  useEffect(() => {
+    if (WebApp) {
+      WebApp.expand();
+    }
+    // if (!divRef.current) return;
+    // const viewport = searchParams.get("viewport");
+    // // alert("TESTTTTT");
+    // // alert(viewport);
+    // console.log("TESTTT");
+    // if (viewport) {
+    //   // alert(`${viewport} HATDOG`);
+    //   console.log("WHY ARE YOU RUNNING?");
+    //   divRef.current.style.height = `${viewport}vh`;
+    //   divRef.current.style.backgroundColor = "red";
+    // }
+    // document.querySelectorAll("input, textarea").forEach((el) => {
+    //   el.addEventListener("focus", () => {
+    //     alert("FOCUSED");
+    //     setTimeout(() => {
+    //       WebApp.expand();
+    //     }, 500);
+    //   });
+    // });
+  }, []);
+
   return (
-    <div>
-      <div>test</div>
+    <div className="container" ref={divRef}>
+      <div className="header">HEADER</div>
+
+      <div className="body">
+        <div>
+          <input type="text" onFocus={onfocus} />
+        </div>
+        <div>
+          <input type="text" onFocus={onfocus} />
+        </div>
+      </div>
+      <div className="footer">FOOTER</div>
     </div>
   );
 };
